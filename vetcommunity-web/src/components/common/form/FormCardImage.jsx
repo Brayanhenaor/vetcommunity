91cf3bd2bcb5decab3208d47d4b25a65fb97c1d6
Brayan Henao Raigosa <bhenao@intergrupo.com>
2022-04-18 18:48:46 -0500

    fix design

--- vetcommunity-web/src/components/common/form/FormCardImage.jsx
+++ vetcommunity-web/src/components/common/form/FormCardImage.jsx
@@ -10,7 +10,7 @@ export const FormCardImage = ({ methods, onSubmit, image, children }) => {
             <form onSubmit={methods.handleSubmit(onSubmit)}>
                 <Box sx={{ bgcolor: color.lightSecondary, minHeight: '100vh' }}>
                     <Grid container alignItems={'center'} sx={{ bgcolor: color.lightSecondary, minHeight: '100vh' }} justifyContent={'center'}>
-                        <Card sx={{ width: '80%', borderRadius: '20px', maxWidth: '95%', overflow:'scroll' }}>
+                        <Card sx={{ width: '80%', borderRadius: '20px', maxWidth: '95%', overflow:'scroll',  maxHeight: '80vh' }}>
                             <Grid container>
                                 <Grid container justifyContent={'center'} alignItems='center' sx={{ padding: 5 }} item md={6}>
                                     <Grid container>
@@ -18,7 +18,7 @@ export const FormCardImage = ({ methods, onSubmit, image, children }) => {
                                     </Grid>
                                 </Grid>
                                 <Grid container item md={6} sx={{ display: { xs: 'none', md: 'flex' } }}>
-                                    <img src={image} alt="pets" style={{ objectFit: 'cover', maxWidth:'100%', minHeight:'70vh', maxHeight: '80vh' }} />
+                                    <img src={image} alt="pets" style={{ objectFit: 'cover', maxWidth:'100%', minHeight:'70vh' }} />
                                 </Grid>
                             </Grid>
                         </Card>
