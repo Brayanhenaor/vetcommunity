8da6923bc2b026638a406e39a808b7ed7a7a2e3e
Brayan Henao Raigosa <bhenao@intergrupo.com>
2022-04-18 18:56:22 -0500

    hide scrollbar

--- vetcommunity-web/src/components/common/form/FormCardImage.jsx
+++ vetcommunity-web/src/components/common/form/FormCardImage.jsx
@@ -1,16 +1,26 @@
 import { Card, Grid } from '@mui/material'
-import { Box } from '@mui/system'
+import { Box, styled } from '@mui/system'
 import React from 'react'
 import { FormProvider } from 'react-hook-form'
 import { color } from '../../../utils/color'
 
+const StyledCard = styled(Card)({
+    width: '80%',
+    borderRadius: '20px',
+    maxWidth: '95%',
+    overflow: 'scroll',
+    maxHeight: '80vh',
+    '&::-webkit-scrollbar':{
+        display:'none'
+    }
+});
 export const FormCardImage = ({ methods, onSubmit, image, children }) => {
     return (
         <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(onSubmit)}>
                 <Box sx={{ bgcolor: color.lightSecondary, minHeight: '100vh' }}>
                     <Grid container alignItems={'center'} sx={{ bgcolor: color.lightSecondary, minHeight: '100vh' }} justifyContent={'center'}>
-                        <Card sx={{ width: '80%', borderRadius: '20px', maxWidth: '95%', overflow:'scroll',  maxHeight: '80vh' }}>
+                        <StyledCard>
                             <Grid container>
                                 <Grid container justifyContent={'center'} alignItems='center' sx={{ padding: 5 }} item md={6}>
                                     <Grid container>
@@ -18,10 +28,10 @@ export const FormCardImage = ({ methods, onSubmit, image, children }) => {
                                     </Grid>
                                 </Grid>
                                 <Grid container item md={6} sx={{ display: { xs: 'none', md: 'flex' } }}>
-                                    <img src={image} alt="pets" style={{ objectFit: 'cover', maxWidth:'100%', minHeight:'70vh' }} />
+                                    <img src={image} alt="pets" style={{ objectFit: 'cover', maxWidth: '100%', minHeight: '70vh' }} />
                                 </Grid>
                             </Grid>
-                        </Card>
+                        </StyledCard>
                     </Grid>
                 </Box>
             </form>
