6831ee62e5fe38fe31d464aa5d152b7c754f6406
Daniel Rodriguez <danielr5283@gmail.com>
2022-05-17 16:10:53 -0500

    Register User

---
+++ vetcommunity-backend/vetcommunity/DTOs/Request/RegisterRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace vetcommunity.DTOs.Request
+{
+	public class RegisterRequest
+	{
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool IsVeterinary { get; set; }
+    }
+}
+
